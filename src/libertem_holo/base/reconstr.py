807327c72f051853946a8669e10369d276f22f85
Alexander Clausen <alex@gc-web.de>
2024-03-15 11:23:33 +0100

    Core reconstruction changes by @patrickadrian7
    - Add different aperture filtering options
    - Add phase ramp compensation helper
    - Simplify line filter
    - Add double resolution reconstruction mode
    - Add phase shifting stack reconstruction
    - Plotting helper function
    
    Co-authored-by: patrickadrian7 <patrickadrian7@gmail.com>

--- src/libertem_holo/base/reconstr.py
+++ src/libertem_holo/base/reconstr.py
@@ -26,6 +26,9 @@
 
 import numpy as np
 from numpy.fft import fft2
+from libertem_holo.base.mask import disk_aperture
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
 
 def freq_array(shape, sampling=(1., 1.)):
@@ -50,42 +53,17 @@ def freq_array(shape, sampling=(1., 1.)):
     return f_freq
 
 
-def aperture_function(r, apradius, rsmooth):
-    """
-    A smooth aperture function that decays from apradius-rsmooth to apradius+rsmooth.
-    Parameters
-    ----------
-    r : ndarray
-        Array of input data (e.g. frequencies)
-    apradius : float
-        Radius (center) of the smooth aperture. Decay starts at apradius - rsmooth.
-    rsmooth : float
-        Smoothness in halfwidth. rsmooth = 1 will cause a decay from 1 to 0 over 2 pixel.
-    Returns
-    -------
-        2d array containing aperture
-    """
-
-    return 0.5 * (1. - np.tanh((np.absolute(r) - apradius) / (0.5 * rsmooth)))
-
-
-def get_aperture(out_shape, sb_size, sb_smoothness, sig_shape):
+def get_slice_fft(out_shape, sig_shape):
     sy, sx = sig_shape
     oy, ox = out_shape
-    f_sampling = (1. / oy, 1. / ox)
-    sb_size = sb_size * np.mean(f_sampling)
-    sb_smoothness = sb_size * sb_smoothness * np.mean(f_sampling)
-
-    f_freq = freq_array(out_shape)
-    aperture = aperture_function(f_freq, sb_size, sb_smoothness)
 
     y_min = int(sy / 2 - oy / 2)
     y_max = int(sy / 2 + oy / 2)
     x_min = int(sx / 2 - ox / 2)
-    x_max = int(sx / 2 + oy / 2)
+    x_max = int(sx / 2 + ox / 2)
     slice_fft = (slice(y_min, y_max), slice(x_min, x_max))
 
-    return slice_fft, aperture
+    return slice_fft
 
 
 def estimate_sideband_position(holo_data, holo_sampling, central_band_mask_radius=None, sb='lower'):
@@ -117,9 +95,10 @@ def estimate_sideband_position(holo_data, holo_sampling, central_band_mask_radiu
     if central_band_mask_radius is None:
         central_band_mask_radius = 1 / 20. * np.max(f_freq)
 
-    aperture = aperture_function(f_freq, central_band_mask_radius, 1e-6)
+    aperture = disk_aperture(holo_data.shape, central_band_mask_radius)
+
     # A small aperture masking out the centerband.
-    aperture_central_band = np.subtract(1.0, aperture)  # 1e-6
+    aperture_central_band = np.subtract(1.0, aperture)
     # imitates 0
 
     fft_holo = fft2(holo_data) / np.prod(holo_data.shape)
@@ -128,10 +107,10 @@ def estimate_sideband_position(holo_data, holo_sampling, central_band_mask_radiu
     # Sideband position in pixels referred to unshifted FFT
     if sb == 'lower':
         fft_sb = fft_filtered[:int(fft_filtered.shape[0] / 2), :]
-        sb_position = np.asarray(np.unravel_index(fft_sb.argmax(), fft_sb.shape))
+        sb_position = np.asarray(np.unravel_index(np.abs(fft_sb).argmax(), fft_sb.shape))
     elif sb == 'upper':
         fft_sb = fft_filtered[int(fft_filtered.shape[0] / 2):, :]
-        sb_position = (np.unravel_index(fft_sb.argmax(), fft_sb.shape))
+        sb_position = (np.unravel_index(np.abs(fft_sb).argmax(), fft_sb.shape))
         sb_position = np.asarray(np.add(sb_position, (int(fft_filtered.shape[0] / 2), 0)))
 
     return sb_position
@@ -178,3 +157,203 @@ def reconstruct_frame(frame, sb_pos, aperture, slice_fft, precision=True):
 
     wav = np.fft.ifft2(fft_frame) * np.prod(frame_size)
     return wav
+
+
+def reconstruct_double_resolution(frame, sb_pos, aperture, slice_fft, precision=True):
+    """
+    Reconstruct a stack of phase shifted holography with double resolution method.
+
+    Parameters
+    ----------
+    Frame : array_like
+        Holographic data array
+    sb_pos : tuple
+        The sideband position (y, x), referred to the non-shifted FFT.
+    aperture : 2D Array
+        array containing the mask for reconstruction
+
+    Returns
+    -------
+    wav : complex array
+        the reconstructed complex image
+
+    """
+    image_double_resolution = frame[1]-frame[0]
+    wav = reconstruct_frame(image_double_resolution,
+                            sb_pos, aperture, slice_fft)
+    return wav
+
+
+def estimate_omega(image, sideband_position, flip=False, stack=True):
+    """
+    Estimates the frequency carrier of the hologram
+
+    Parameters
+    ----------
+    image : array
+            Holographic data array
+    sb_position : tuple
+        The sideband position (y, x), referred to the non-shifted FFT.
+
+    Returns
+    -------
+    omega : tuple
+        frequency carrier in y and x axis
+
+    """
+    if stack:
+        width = image.shape[2]
+    else:
+        width = image.shape[1]
+
+    if sideband_position[1] >= width / 2:
+        omega = (-sideband_position[0], (width-sideband_position[1]))
+    elif sideband_position[1] < width / 2:
+        omega = (-sideband_position[0], sideband_position[1])
+
+    if flip:
+        omega = (-omega[0], -omega[1])
+
+    return omega
+
+
+def reconstruct_direct_euler(image, omega):
+    """
+    Reconstruct a stack of phase shifted holography with direct
+    reconstruction method by Ru et.al. 1994 (euler form)
+
+    Parameters
+    ----------
+    image : array
+        Holographic data array
+    omega: tuple
+        frequency carrier in y and x axis
+
+    Returns
+    -------
+    phase final : 2D array
+        the reconstructed phase image
+
+    """
+    number_of_images = image.shape[1]
+    phase_initial_euler = np.zeros(number_of_images, dtype="complex128")
+
+    n = np.arange(number_of_images)
+    phase_initial = 2*np.pi*n/number_of_images
+    phase_initial_euler = np.exp(1j*phase_initial)
+
+    c22 = 0
+    for i in range(number_of_images):
+        c22 = c22 + (image[i] * phase_initial_euler[i])
+    c22 = c22 / number_of_images
+
+    x = np.linspace(0, omega[1], image.shape[2], endpoint=False)
+    y = np.linspace(0, omega[0], image.shape[1], endpoint=False)
+    irow, icol = np.meshgrid(x, y, indexing='xy')
+
+    ramp_carrier = np.exp(1j*2*np.pi*(irow+icol))
+
+    c22 = c22 / ramp_carrier
+    phase_final = np.angle(c22)
+    return phase_final
+
+
+def reconstruct_direct(frame, number_of_images, omega):
+    """
+    Reconstruct a stack of phase shifted holography with direct
+    reconstruction method
+
+    Parameters
+    ----------
+    Frame : array_like
+        Holographic data array
+    number_of_images : int
+        The number of images inside the stack
+    omega: tuple
+        frequency carrier in y and x axis
+
+    Returns
+    -------
+    phase final : 2D array
+        the reconstructed phase image
+
+    """
+    xspace = omega[1]
+    yspace = omega[0]
+    compfront = np.zeros((frame.shape[1], frame.shape[2]), dtype="complex64")
+    compcar = np.zeros((frame.shape[1], frame.shape[2]), dtype="complex64")
+    sin_value_sum = np.zeros((frame.shape[1], frame.shape[2]), dtype="complex64")
+    cos_value_sum = np.zeros((frame.shape[1], frame.shape[2]), dtype="complex64")
+    coscar = np.zeros((frame.shape[1], frame.shape[2]), dtype="complex64")
+    sincar = np.zeros((frame.shape[1], frame.shape[2]), dtype="complex64")
+
+    cos_value_sum = np.array(0)
+    sin_value_sum = np.array(0)
+    n = np.arange(number_of_images)
+    initial_phase_change = 2 * np.pi * (n/number_of_images)
+
+    for i in range(number_of_images):
+        cos_value = frame[i] * np.cos(initial_phase_change[i])
+        sin_value = frame[i] * np.sin(initial_phase_change[i])
+        cos_value_sum = cos_value_sum + cos_value
+        sin_value_sum = sin_value_sum + sin_value
+    compfront = cos_value_sum + sin_value_sum * 1j
+
+    x = np.linspace(0, xspace, frame.shape[2], endpoint=False)
+    y = np.linspace(0, yspace, frame.shape[1], endpoint=False)
+    irow, icol = np.meshgrid(x, y, indexing='xy')
+
+    coscar = np.cos(2 * np.pi * (icol+irow)).astype('float32')
+    sincar = np.sin(2 * np.pi * (icol+irow)).astype('float32')
+    compcar = coscar + sincar * 1j
+
+    compfinal = compfront/compcar
+    phase_final = np.arctan2(np.imag(compfinal), np.real(compfinal))
+
+    return phase_final
+
+
+def display_fft_image(image, sb_position, slice_fft, mask=1, detail=True):
+    """
+    Display the fft image
+    This function helps to show the steps of the reconstruction and to define
+    the best length and width of line mask
+
+    Parameters
+    ----------
+    image : array
+        the input image
+    sb_position : tuple
+        the sideband position (y, x), referred to the non-shifted FFT.
+    slice fft : array
+        contain minimum and maximum value in y and x axis.
+    mask : array
+        contain the aperture and line mask
+    detail : bolean
+    """
+    fft_original_image = np.fft.fft2(image) / np.prod(image.shape)
+    fft_original_image1 = np.roll(fft_original_image, sb_position, axis=(0, 1))
+    fft_original_image2 = np.fft.fftshift(fft_original_image1)
+    fft_original_image3 = fft_original_image2[slice_fft]
+    fft_original_image4 = np.fft.fftshift(fft_original_image3)
+    fft_with_aperture = fft_original_image4 * mask
+    if detail:
+        plt.figure()
+        plt.imshow(np.abs(fft_original_image1), norm=LogNorm(), cmap="gray")
+        plt.figure()
+        plt.imshow(np.abs(fft_original_image2), norm=LogNorm(), cmap="gray")
+        plt.figure()
+        plt.imshow(np.abs(fft_original_image3), norm=LogNorm(), cmap="gray")
+        plt.figure()
+        plt.imshow(np.abs(fft_original_image4), norm=LogNorm(), cmap="gray")
+        figure, (ax1, ax2) = plt.subplots(1, 2)
+        ax1.imshow(np.abs(fft_original_image4), norm=LogNorm(), cmap="gray")
+        ax1.set_title('Without Aperture Mask')
+        ax2.imshow(np.log1p(np.abs(fft_with_aperture)), cmap="gray")
+        ax2.set_title('FFT with Aperture Mask')
+    else:
+        figure, (ax1, ax2) = plt.subplots(1, 2)
+        ax1.imshow(np.abs(fft_original_image4), norm=LogNorm(), cmap="gray")
+        ax1.set_title('Without Aperture Mask')
+        ax2.imshow(np.abs(fft_with_aperture), vmax=0.01, cmap="gray")
+        ax2.set_title('FFT with Aperture Mask')
