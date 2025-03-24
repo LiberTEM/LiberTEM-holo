b7badb24925664fe71b8429b0a18309d65c21aeb
Alexander Clausen <a.clausen@fz-juelich.de>
2025-03-24 14:48:37 +0100

    Small stack alignment fixes; add benchmark

--- benchmarks/test_holo_stack.py
+++ benchmarks/test_holo_stack.py
@@ -1,9 +1,11 @@
 import pytest
+import numpy as np
 
 from libertem.api import Context
 from libertem.utils.devices import detect
 from libertem.common.backend import set_use_cpu, set_use_cuda
 
+from libertem_holo.base.align import align_stack
 from libertem_holo.udf import HoloReconstructUDF
 
 
@@ -85,3 +87,46 @@ def test_stack_reconstr_parallel(
             precision=True,
         )
         benchmark(ctx.run_udf, udf=udf, dataset=ds)
+
+
+@pytest.mark.benchmark(
+    group="stack"
+)
+@pytest.mark.parametrize(
+    'backend', ['numpy', 'cupy'],
+)
+def test_stack_alignment(
+    backend, benchmark, large_holo_data, gpu_ctx, cpu_ctx
+):
+    path, _ = large_holo_data
+    out_shape = (1024, 1024)
+    if backend == 'cupy':
+        if gpu_ctx is None:
+            pytest.skip("No CUDA device or no CuPy, skipping CuPy test")
+        ctx = gpu_ctx
+        import cupy as xp
+    else:
+        ctx = cpu_ctx
+        xp = np
+
+    ds = ctx.load("npy", str(path))
+
+    with ctx:
+        udf = HoloReconstructUDF.with_default_aperture(
+            out_shape=out_shape,
+            sb_size=out_shape[0] - 32,
+            sb_position=(1024, 1024),
+            precision=True,
+        )
+        result = ctx.run_udf(dataset=ds, udf=udf)
+        wave_stack = result['wave'].data.reshape((-1, 1024, 1024))
+        amp_stack = np.abs(wave_stack)
+
+        benchmark(
+            align_stack,
+            stack=amp_stack,
+            wave_stack=wave_stack,
+            static=None,
+            correlator=None,
+            xp=xp,
+        )
