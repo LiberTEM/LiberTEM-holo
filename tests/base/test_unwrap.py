ca47bc6f07c71522eab0192c8706cbcb642abe99
Alexander Clausen <a.clausen@fz-juelich.de>
2026-06-23 20:13:11 +0200

    Coverage data for `quality_unwrap`

--- tests/base/test_unwrap.py
+++ tests/base/test_unwrap.py
@@ -1,7 +1,9 @@
+import pytest
 import numpy as np
 from libertem_holo.base.unwrap import derivative_variance, quality_unwrap
 
 
+@pytest.mark.with_numba
 def test_quality_unwrap_smoketest():
     phase = np.random.random(128*128).reshape(128, 128)
     dv = derivative_variance(phase)
