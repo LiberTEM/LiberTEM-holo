760a4e8d5511c7dd743bfcbd1d7c66f3861ca332
Alexander Clausen <a.clausen@fz-juelich.de>
2026-06-29 17:14:00 +0200

    Updates for convenience code; run notebook tests on GPU

--- src/libertem_holo/base/convenience.py
+++ src/libertem_holo/base/convenience.py
@@ -1,64 +1,65 @@
-from libertem_holo.base.io import Results
+"""Convenience functions for reconstruction and visualization."""
+from scipy.ndimage import gaussian_filter
+
+from libertem_holo.base.align import Correlator, ImageCorrelator
+from libertem_holo.base.io import InputData, Results
+from libertem_holo.base.reconstr import (
+    phase_offset_correction,
+    reconstruct_bf,
+    reconstruct_frame,
+)
 from libertem_holo.base.utils import HoloParams
-from libertem_holo.base.reconstr import reconstruct_frame, reconstruct_bf
-from libertem_holo.base.align import ImageCorrelator, Correlator
-from libertem_holo.base.reconstr import phase_offset_correction
-from scipy.ndimage import shift, gaussian_filter
+
 try:
+    import cupy as cp
     from cupyx.scipy.ndimage import shift as shiftcp
 except ImportError:
+    cp = None
     shiftcp = None
-from skimage.measure import block_reduce as br
-from matplotlib.axes import Axes
+
 import empyre as emp
 import numpy as np
+from matplotlib.axes import Axes
+from skimage.measure import block_reduce as br
 from tqdm import tqdm
-import typing
-XPType = typing.Any  # Union[Module("numpy"), Module("cupy")]
 
 
 def reconstruct_stack(
-    stack: np.ndarray,
-    stack_ref: np.ndarray = None,
-    holoparams: HoloParams = None,
-    correlator: Correlator = None,
-    xp: XPType = np,
-) -> tuple:
-    """Reconstruct a stack of holograms, aligning them
-    and optionally using a reference stack for phase correction.
+    stack: InputData,
+    stack_ref: InputData | None = None,
+    holoparams: HoloParams | None = None,
+    correlator: Correlator | None = None,
+) -> Results:
+    """Reconstruct and align a stack of holograms.
+
+    Optionally use a reference stack for phase correction.
 
     Parameters
     ----------
-    stack : np.ndarray
+    stack : InputData
         The input stack of holograms to reconstruct.
         Shape should be (N, H, W) or (H, W).
-    stack_ref : np.ndarray, optional
+    stack_ref : InputData, optional
         An optional reference stack of holograms for phase correction.
         Shape should be (N, H, W) or (H, W).
     holoparams : HoloParams, optional
         Parameters for hologram reconstruction.
-        If None, parameters will be estimated from the first hologram in the stack.
+        If None, parameters will be estimated from
+        the first hologram in the stack.
     correlator : Correlator, optional
         Parameters for frame by frame alignment.
         If None, phase correlation will be used.
-    xp : module, optional
-        The array module to use for computations.
-        Can be numpy or cupy. Default is numpy.
 
     Returns
     -------
-    wave : np.ndarray
-        The reconstructed complex wave.
-    bf_avg : np.ndarray
-        The averaged bright-field image.
-    holoparams : HoloParams
-        The holography reconstruction parameters.
-    px_size : float
-        The pixel size in the reconstructed wave.
+    results
+        The results, packaged up in a Results instance
 
     """
-    if shiftcp is None:
-        raise RuntimeError("Need Cupy for this stack reconstruction function.")
+    if shiftcp is None or cp is None:
+        msg = "Need Cupy for this stack reconstruction function."
+        raise RuntimeError(msg)
+    xp = cp
     sig_shape = stack.data[0].shape
     out_shape = (sig_shape[0]//8, sig_shape[1]//8)
     if holoparams is None:
@@ -74,25 +75,32 @@ def reconstruct_stack(
     if correlator is None:
         correlator = ImageCorrelator(
             hanning=True,
-            normalization='phase',
+            normalization="phase",
             binning=2,
             upsample_factor=10,
             xp=xp,
         )
-    waves_aligned = xp.zeros((len(stack.data),) + holoparams.out_shape, dtype=np.complex128)
-    bfs_aligned = xp.zeros((len(stack.data),) + holoparams.out_shape, dtype=np.float32)
+    stack_out_shape = (len(stack.data),) + holoparams.out_shape
+    waves_aligned = xp.zeros(stack_out_shape, dtype=np.complex128)
+    bfs_aligned = xp.zeros(stack_out_shape, dtype=np.float32)
     drifts = xp.zeros((len(stack.data), 2))
+
     for i in tqdm(range(len(stack.data))):
         obj = stack.data[i]
 
         wave_obj = reconstruct_frame(
-            frame=obj, sb_pos=holoparams.sb_position,
-            aperture=holoparams.aperture, slice_fft=holoparams.slice_fft,
+            frame=obj,
+            sb_pos=holoparams.sb_position,
+            aperture=holoparams.aperture,
+            slice_fft=holoparams.slice_fft,
             xp=xp,
         )
         if i == 0:
             bf_obj_0 = reconstruct_bf(
-                frame=obj, aperture=holoparams.aperture, slice_fft=holoparams.slice_fft, xp=xp
+                frame=obj,
+                aperture=holoparams.aperture,
+                slice_fft=holoparams.slice_fft,
+                xp=xp,
             )
             f1 = correlator.prepare_input(bf_obj_0)
 
@@ -107,13 +115,8 @@ def reconstruct_stack(
         corr_res = correlator.correlate(ref_image=f1, moving_image=f2)
         shifts = corr_res.shift
 
-        if wave_obj.device == 'cpu':
-            shifts = tuple(float(x) for x in corr_res.shift)
-            wave_shifted = shift(wave_obj, shifts)
-            bf_shifted = shift(bf_obj, shifts)
-        else:
-            wave_shifted = shiftcp(wave_obj, shifts)
-            bf_shifted = shiftcp(bf_obj, shifts)
+        wave_shifted = shiftcp(wave_obj, shifts)
+        bf_shifted = shiftcp(bf_obj, shifts)
 
         waves_aligned[i] = wave_shifted
         bfs_aligned[i] = np.abs(bf_shifted)
@@ -125,7 +128,10 @@ def reconstruct_stack(
     if stack_ref is None:
         wave_ref = np.ones_like(wave_avg)
     else:
-        waves_ref = xp.zeros((len(stack_ref.data),) + holoparams.out_shape, dtype=np.complex128)
+        waves_ref = xp.zeros(
+            (len(stack_ref.data),) + holoparams.out_shape,
+            dtype=np.complex128,
+        )
 
         for i in tqdm(range(len(stack_ref.data))):
             ref = stack_ref.data[i]
@@ -141,8 +147,8 @@ def reconstruct_stack(
     wave = wave_avg / wave_ref
     res = Results(complex_wave=wave.get(), brightfield=bf_avg.get())
     res.metadata_from_input(stack, holoparams)
-    res.metadata['drifts_x'] = list(drifts[..., 0].get())
-    res.metadata['drifts_y'] = list(drifts[..., 1].get())
+    res.metadata["drifts_x"] = list(drifts[..., 0].get())
+    res.metadata["drifts_y"] = list(drifts[..., 1].get())
     return res
 
 
@@ -207,6 +213,7 @@ def plot_mag_induction(
     - cosine contours of the smoothed phase image,
     - a color wheel indicating the direction encoding,
     - an optional white contour showing the supplied mask.
+
     """
     phase_binned = br(phase, (binning, binning), np.mean)
 
